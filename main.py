b743384ffbd6256760c6a447acdd0608582422b1
Quentin Brillais <qbrillai@c2r6p7.42nice.fr>
2025-04-08 14:50:50 +0200

    new

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import json
 
 def linear_regression():
     km = input("Entrez un kilomètrage:\n")
-    if km.isdigit():
+    if km.isdigit() and float(km) >= 0:
         if os.path.exists('data.json'):
             with open('data.json', 'r') as json_file:
                 existing_data = json.load(json_file)
                 theta0 = existing_data['theta0']
                 theta1 = existing_data['theta1']
                 json_file.close()
+
+                estimated_price = float(km) * float(theta1) + float(theta0)
+                print(f"Estimated price is: {estimated_price}$")
         else :
             with open('data.json', 'w') as json_file:
                 json.dump({'theta0': '0', 'theta1' : '0'}, json_file)
                 json_file.close()
 
     else:
-        print("Merci d entrer une valeur chiffrée")
+        print("Merci d entrer une valeur chiffrée et positive")
         exit()
-
         
 if __name__ == '__main__':
     linear_regression()
--- train.py
+++ train.py
@@ -4,41 +4,59 @@ import os.path
 import pandas as pd
 import json
 
-
 def train():
     data = pd.read_csv("data.csv")
-    learning_rate = 0.1
+    learning_rate = 0.01
     m = len(data.km.values)
     the0 = 0.0
-    the1 = 0.1
+    the1 = 0.0
+    overfit_reached = 0
+    overfit = 0
     min_km = min((data.km))
     max_km = max((data.km))
     max_price = max((data.price))
     min_price = min((data.price))
-    for i in range(1000):
+    prev_the0_rounded = 0
+    prev_the1_rounded = 0
+    for i in range(100000):
         sum_t0 = 0.0
         sum_t1 = 0.0
         for value in data.values:
-            #Utilisation de la normalisation selon la formule: X_norm = (X - X.min()) / (X.max() - X.min())
-            #La normalisation est utile pour reduire l ecart entre les donnees en les contenant toutes entre 0 et 1. Un trop grand ecart peut fausser l apprentissage.
             value_0 = (float(value[0]) - min_km) / (max_km - min_km)
             value_1 = (float(value[1]) - min_price) / (max_price - min_price)
-            #On additionne le total de nos erreurs dans la prediction du prix du vehicule pour se faire on applique la formule estimatePrice(mileage) = θ0 + (θ1 ∗ mileage) puis on soustrait le vrai prix pour comparer pour chaque index.
             sum_t0 = sum_t0 + (the0 + (the1 * value_0) - value_1)
-            # Le calcul pour la sum_t1 est le meme mais on multiplie par le nombre de kilometres a la fin pour estimer la participation du kilometrage a notre erreur.
             sum_t1 = sum_t1 + (the0 + (the1 * value_0) - value_1) * value_0
         
-        #Normalisation de learning_rate et calcul des valeurs temporaires de theta0 et theta1 en fonction de la marge d erreur trouvee.
         tmp_the0 = learning_rate * 1/m * sum_t0
         tmp_the1 = learning_rate * 1/m * sum_t1
 
-        #Ajustement des valeurs initiales avant de reboucler sur les valeurs et continuer de les peaufiner encore et encore.
         the0 -= tmp_the0
         the1 -= tmp_the1
 
-    #Denormalisation des donnees, pour les ramener a leur echelle initiale.
+        the0_rounded = round(the0, 5)
+        the1_rounded = round(the1, 5)
+
+        if the0_rounded == prev_the0_rounded or the1_rounded == prev_the1_rounded:
+            overfit += 1
+        else :
+            overfit = 0
+        if overfit == 20:
+            overfit_reached = 1
+            print(f"Overfit detected, early stopping triggered after {i} loops")
+        prev_the0_rounded = the0_rounded
+        prev_the1_rounded = the1_rounded
+        if overfit_reached == 1:
+            break
+
     the0 = the0 * (max_price - min_price) + min_price - (the1 * min_km * (max_price - min_price)) / (max_km - min_km)
     the1 = the1 * (max_price - min_price) / (max_km - min_km)
+
+    if os.path.exists('data.json'):
+            with open('data.json', 'r') as json_file:
+                old_data = json.load(json_file)
+                old_theta0 = old_data['theta0']
+                old_theta1 = old_data['theta1']
+                json_file.close()
 
     with open('data.json', 'w') as json_file:
         json.dump({'theta0': the0, 'theta1' : the1}, json_file)
@@ -48,10 +66,11 @@ def train():
 
     plt.scatter(data['km'], data['price'])
     plt.title('Repartition du prix selon le kilometrage')
-    #Tracage de la ligne de regression qui est la relation entre les deux variables.
-    plot_x = data.km
+    plot_x = data.km.values
     plot_y = the1 * plot_x + the0
-    plt.plot(plot_x, plot_y, '-g')
+    plot_y_old = old_theta1 * plot_x + old_theta0
+    plt.plot(plot_x, plot_y_old, '-r', label='Ancien modèle')
+    plt.plot(plot_x, plot_y, '-g', label='Nouveau modèle')
     plt.xlabel('Kilometres')
     plt.ylabel('Prix')
     plt.show()
